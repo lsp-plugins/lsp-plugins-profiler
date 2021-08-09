19d70b4087307d2d7fa3eea4749f6852ab622fe5
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-08-09 20:59:16 +0300

    Updated project metadata

--- project.mk
+++ project.mk
@@ -21,7 +21,7 @@
 # Package version
 ARTIFACT_ID                 = LSP_PLUGINS_PROFILER
 ARTIFACT_NAME               = lsp-plugins-profiler
-ARTIFACT_DESC               = Trigger plugin series
+ARTIFACT_DESC               = Profiler plugin series
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
 ARTIFACT_VERSION            = 1.0.1-devel
