1c7d68459c848e66a6cbf97255023085f9ffa9c7
Vladimir Sadovnikov <sadko4u@gmail.com>
2022-07-10 22:31:10 +0300

    C++ 11 support

--- include/private/meta/profiler.h
+++ include/private/meta/profiler.h
@@ -32,47 +32,47 @@ namespace lsp
     {
         struct profiler_metadata
         {
-            static const float FREQUENCY_MIN        = 20.0f;
-            static const float FREQUENCY_MAX        = 20000.0f;
-            static const float FREQUENCY_DFL        = 1000.0f;
-            static const float FREQUENCY_STEP       = 0.01f;
+            static constexpr float FREQUENCY_MIN        = 20.0f;
+            static constexpr float FREQUENCY_MAX        = 20000.0f;
+            static constexpr float FREQUENCY_DFL        = 1000.0f;
+            static constexpr float FREQUENCY_STEP       = 0.01f;
 
-            static const float AMPLITUDE_DFL        = 1.0f;
+            static constexpr float AMPLITUDE_DFL        = 1.0f;
 
-            static const float LATENCY_MIN          = 0.0f;         /* Min detectable latency [ms] */
-            static const float LATENCY_MAX          = 2000.0f;      /* Max detectable latency [ms] */
-            static const float LATENCY_DFL          = 1000.0f;
-            static const float LATENCY_STEP         = 1.0f;
+            static constexpr float LATENCY_MIN          = 0.0f;         /* Min detectable latency [ms] */
+            static constexpr float LATENCY_MAX          = 2000.0f;      /* Max detectable latency [ms] */
+            static constexpr float LATENCY_DFL          = 1000.0f;
+            static constexpr float LATENCY_STEP         = 1.0f;
 
-            static const float PEAK_THRESHOLD_MIN   = GAIN_AMP_M_84_DB;
-            static const float PEAK_THRESHOLD_MAX   = GAIN_AMP_0_DB;
-            static const float PEAK_THRESHOLD_DFL   = GAIN_AMP_M_24_DB;
-            static const float PEAK_THRESHOLD_STEP  = 0.01f;
+            static constexpr float PEAK_THRESHOLD_MIN   = GAIN_AMP_M_84_DB;
+            static constexpr float PEAK_THRESHOLD_MAX   = GAIN_AMP_0_DB;
+            static constexpr float PEAK_THRESHOLD_DFL   = GAIN_AMP_M_24_DB;
+            static constexpr float PEAK_THRESHOLD_STEP  = 0.01f;
 
-            static const float ABS_THRESHOLD_MIN    = GAIN_AMP_M_84_DB;
-            static const float ABS_THRESHOLD_MAX    = GAIN_AMP_0_DB;
-            static const float ABS_THRESHOLD_DFL    = GAIN_AMP_M_24_DB;
-            static const float ABS_THRESHOLD_STEP   = 0.01f;
+            static constexpr float ABS_THRESHOLD_MIN    = GAIN_AMP_M_84_DB;
+            static constexpr float ABS_THRESHOLD_MAX    = GAIN_AMP_0_DB;
+            static constexpr float ABS_THRESHOLD_DFL    = GAIN_AMP_M_24_DB;
+            static constexpr float ABS_THRESHOLD_STEP   = 0.01f;
 
-            static const float DURATION_MIN         = 1.0f;
-            static const float DURATION_MAX         = 50.0f;
-            static const float DURATION_DFL         = 10.0f;
-            static const float DURATION_STEP        = 0.5f;
+            static constexpr float DURATION_MIN         = 1.0f;
+            static constexpr float DURATION_MAX         = 50.0f;
+            static constexpr float DURATION_DFL         = 10.0f;
+            static constexpr float DURATION_STEP        = 0.5f;
 
-            static const float MTR_T_MIN            = 0.0f;
-            static const float MTR_T_MAX            = 60.0f;
-            static const float MTR_T_DFL            = 0.0f;
-            static const float MTR_T_STEP           = 1.0f;
+            static constexpr float MTR_T_MIN            = 0.0f;
+            static constexpr float MTR_T_MAX            = 60.0f;
+            static constexpr float MTR_T_DFL            = 0.0f;
+            static constexpr float MTR_T_STEP           = 1.0f;
 
-            static const float IR_OFFSET_MIN        = -1000.0f;
-            static const float IR_OFFSET_MAX        = 1000.0f;
-            static const float IR_OFFSET_DFL        = 0.0f;
-            static const float IR_OFFSET_STEP       = 0.01f;
+            static constexpr float IR_OFFSET_MIN        = -1000.0f;
+            static constexpr float IR_OFFSET_MAX        = 1000.0f;
+            static constexpr float IR_OFFSET_DFL        = 0.0f;
+            static constexpr float IR_OFFSET_STEP       = 0.01f;
 
-            static const float MTR_LATENCY_MIN      = 0.0f;         /* Min detectable latency [ms] */
-            static const float MTR_LATENCY_MAX      = 2000.0f;      /* Max detectable latency [ms] */
-            static const float MTR_LATENCY_DFL      = 0.0f;
-            static const float MTR_LATENCY_STEP     = 1.0f;
+            static constexpr float MTR_LATENCY_MIN      = 0.0f;         /* Min detectable latency [ms] */
+            static constexpr float MTR_LATENCY_MAX      = 2000.0f;      /* Max detectable latency [ms] */
+            static constexpr float MTR_LATENCY_DFL      = 0.0f;
+            static constexpr float MTR_LATENCY_STEP     = 1.0f;
 
             enum rt_algorithm_selector_t
             {
@@ -85,20 +85,20 @@ namespace lsp
                 SC_RTALGO_DFL = SC_RTALGO_T_20
             };
 
-            static const float MTR_RT_MIN           = 0.0f;
-            static const float MTR_RT_MAX           = 60.0f;
-            static const float MTR_RT_DFL           = 0.0f;
-            static const float MTR_RT_STEP          = 1.0f;
+            static constexpr float MTR_RT_MIN           = 0.0f;
+            static constexpr float MTR_RT_MAX           = 60.0f;
+            static constexpr float MTR_RT_DFL           = 0.0f;
+            static constexpr float MTR_RT_STEP          = 1.0f;
 
-            static const float MTR_IL_MIN           = 0.0f;
-            static const float MTR_IL_MAX           = 60.0f;
-            static const float MTR_IL_DFL           = 0.0f;
-            static const float MTR_IL_STEP          = 1.0f;
+            static constexpr float MTR_IL_MIN           = 0.0f;
+            static constexpr float MTR_IL_MAX           = 60.0f;
+            static constexpr float MTR_IL_DFL           = 0.0f;
+            static constexpr float MTR_IL_STEP          = 1.0f;
 
-            static const float MTR_R_MIN            = -1.0f;
-            static const float MTR_R_MAX            = 1.0f;
-            static const float MTR_R_DFL            = 0.0f;
-            static const float MTR_R_STEP           = 0.001f;
+            static constexpr float MTR_R_MIN            = -1.0f;
+            static constexpr float MTR_R_MAX            = 1.0f;
+            static constexpr float MTR_R_DFL            = 0.0f;
+            static constexpr float MTR_R_STEP           = 0.001f;
 
             enum saving_mode_selector_t
             {
@@ -111,7 +111,7 @@ namespace lsp
                 SC_SVMODE_DFL = SC_SVMODE_AUTO
             };
 
-            static const size_t RESULT_MESH_SIZE    = 512;
+            static constexpr size_t RESULT_MESH_SIZE    = 512;
         };
 
         extern const meta::plugin_t profiler_mono;
