dbd150a006dc5420071f8ef68fe8b8f807ea9954
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-04 23:45:17 +0300

    Using new mechanism for registering plugin metadata

--- src/main/meta/profiler.cpp
+++ src/main/meta/profiler.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugins-profiler
  * Created on: 3 авг. 2021 г.
@@ -20,6 +20,7 @@
  */
 
 #include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <lsp-plug.in/plug-fw/meta/registry.h>
 #include <lsp-plug.in/shared/meta/developers.h>
 #include <lsp-plug.in/common/status.h>
 #include <private/meta/profiler.h>
@@ -182,6 +183,7 @@ namespace lsp
             mono_plugin_port_groups,
             &profiler_bundle
         };
+        LSP_REGISTER_METADATA(profiler_mono);
 
         const meta::plugin_t profiler_stereo =
         {
@@ -212,5 +214,7 @@ namespace lsp
             stereo_plugin_port_groups,
             &profiler_bundle
         };
+        LSP_REGISTER_METADATA(profiler_stereo);
+
     } /* namespace meta */
 } /* namespace lsp */
